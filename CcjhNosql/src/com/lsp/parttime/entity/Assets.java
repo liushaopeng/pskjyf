e96992c14214e3a100fd97b0f9165cfb3f5a5135
Administrator <Administrator@chongzi>
2017-08-29 16:33:15 +0800

    兼职

--- CcjhNosql/src/com/lsp/parttime/entity/Assets.java
+++ CcjhNosql/src/com/lsp/parttime/entity/Assets.java
@@ -1,5 +1,7 @@
 package com.lsp.parttime.entity;
 
+import java.util.Date;
+
 import com.mongodb.ReflectionDBObject;
 /**
  * 资产
@@ -8,4 +10,36 @@ import com.mongodb.ReflectionDBObject;
  */
 public class Assets extends ReflectionDBObject{
 
+	private String custid;
+	private String formid;
+	private double value;
+	/**
+	 * 更新时间
+	 */
+	private Date update;
+	public String getCustid() {
+		return custid;
+	}
+	public void setCustid(String custid) {
+		this.custid = custid;
+	}
+	public String getFormid() {
+		return formid;
+	}
+	public void setFormid(String formid) {
+		this.formid = formid;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	public Date getUpdate() {
+		return update;
+	}
+	public void setUpdate(Date update) {
+		this.update = update;
+	}
+	
 }
--- CcjhNosql/src/com/lsp/parttime/entity/Order.java
+++ CcjhNosql/src/com/lsp/parttime/entity/Order.java
@@ -1,4 +1,6 @@
-package com.lsp.parttime.entity;
+package com.lsp.parttime.entity; 
+
+import java.util.Date;
 
 import com.mongodb.ReflectionDBObject;
 /**
@@ -8,4 +10,117 @@ import com.mongodb.ReflectionDBObject;
  */
 public class Order extends ReflectionDBObject{
 
+	private String custid;
+	private String fromid;
+	private Date createdate;
+	/**
+	 * 公司ID
+	 */
+	private Long comid;
+	/**
+	 * 任务ID
+	 */
+	private Long mid;
+	/**
+	 * 状态0.已报名1.已拒绝2.进行中3.已完成4.已结算
+	 */
+	private int state;
+	/**
+	 * 金额
+	 */
+	private double amount;
+	/**
+	 * 集合时间
+	 */
+	private Date gatherdate;
+	/**
+	 * 上班开始时间
+	 */
+	private Date startdate;
+	/**
+	 * 上班结束时间
+	 */
+	private Date enddate;
+	/**
+	 * 预计工作时长expectworkinghours
+	 */
+	private int  expectwh;
+	/**
+	 * 实际工作时长
+	 */
+	private int  practicalwh;
+	
+	public Date getGatherdate() {
+		return gatherdate;
+	}
+	public void setGatherdate(Date gatherdate) {
+		this.gatherdate = gatherdate;
+	}
+	public Date getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
+	}
+	public Date getEnddate() {
+		return enddate;
+	}
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
+	}
+	public String getCustid() {
+		return custid;
+	}
+	public void setCustid(String custid) {
+		this.custid = custid;
+	}
+	public String getFromid() {
+		return fromid;
+	}
+	public void setFromid(String fromid) {
+		this.fromid = fromid;
+	}
+	public Date getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+	public Long getComid() {
+		return comid;
+	}
+	public void setComid(Long comid) {
+		this.comid = comid;
+	}
+	public Long getMid() {
+		return mid;
+	}
+	public void setMid(Long mid) {
+		this.mid = mid;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getExpectwh() {
+		return expectwh;
+	}
+	public void setExpectwh(int expectwh) {
+		this.expectwh = expectwh;
+	}
+	public int getPracticalwh() {
+		return practicalwh;
+	}
+	public void setPracticalwh(int practicalwh) {
+		this.practicalwh = practicalwh;
+	}
+	
 }
